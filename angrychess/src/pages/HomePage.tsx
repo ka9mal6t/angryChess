b552f1ef7783725fefc50923890fb9dd393e46d4
ka9mal6tik <vladimyr.kilko@gmail.com>
2024-08-02 17:27:49 +0300

    add burger menu

--- angrychess/src/pages/HomePage.tsx
+++ angrychess/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate, Link} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import { getInfo, startSearchGame, stopSearchGame, checkStatusGame} from '../api/auth';
 import {updateRating} from '../functional/raiting'
 import LoadingComponent from '../components/elements/LoadingComponent'
--- angrychess/src/pages/NotFoundPage.tsx
+++ angrychess/src/pages/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Cookies from 'js-cookie';
 import { useState, useEffect  } from 'react';
+import { useNavigate} from 'react-router-dom';
 
 import bird from './img/bird-bumerang.png'
 import pig from './img/king-pig-wo-eyes.png'
@@ -8,6 +9,7 @@ import eyes_png from './img/eyes.png'
 import './css/Error.css'
 
 const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
 
   useEffect(() => {
     const storedTheme = Cookies.get('theme');
@@ -15,14 +17,29 @@ const NotFoundPage: React.FC = () => {
     document.body.className = result ? 'dark-theme' : '';
   }, []);
 
+  useEffect(() => {
+    const image = document.getElementById("image-error") as HTMLInputElement;
+    
+
+    const imageEvent = () => {
+      navigate('/');
+    };
+
+    image.addEventListener('click', imageEvent);
+    return () => {
+      image.removeEventListener('click', imageEvent);
+    };
+
+  },[]);
+
 
   return (
     <div>
       <main>
         <div className="error">
             <p className="error__p">4</p>
-            <img src={bird} draggable="false" alt="0" className="error__img-light"/>
-            <img src={pig} draggable="false" alt="0" className="error__img-dark"/>
+            <img src={bird} draggable="false" id="image-error" alt="0" className="error__img-light"/>
+            <img src={pig} draggable="false" id="image-error" alt="0" className="error__img-dark"/>
             <img src={eyes_png} draggable="false" alt="0" id="eyes"/>
             <p className="error__p">4</p>
         </div>
